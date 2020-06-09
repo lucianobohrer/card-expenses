23f7b51f18cffc8bc73775270c8992064ca7c9b3
Luciano Bohrer <lucianobohrer@ynap.com>
2020-06-10 00:13:13 +0100

    Initial Commit

---
+++ card-expenses/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  card-expenses
+//
+//  Created by Luciano Bohrer on 10/06/2020.
+//  Copyright © 2020 Luciano Bohrer. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
